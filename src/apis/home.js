025e2fcadd1d363e4989a96b8614ef6bef910588
itheima <itheima@itcast.cn>
2023-05-29 15:03:06 +0800

    人气推荐模块及图片懒加载功能

--- src/apis/home.js
+++ src/apis/home.js
@@ -17,3 +17,14 @@ export const findNewAPI = () => {
     url: '/home/new'
   })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findHotAPI = () => {
+  return httpInstance({
+    url: '/home/hot'
+  })
+}
--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 引入懒加载指令插件并注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
